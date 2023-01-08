8f0ae90988a75f5263bd71e605fa7585246a3e5f
Vlad <62696158+beyondthecode-bc@users.noreply.github.com>
2023-01-08 22:23:19 +0200

    update

--- sections/About.jsx
+++ sections/About.jsx
@@ -2,6 +2,8 @@
 
 import { motion } from 'framer-motion';
 import { TypingText } from '../components';
+import Image from 'next/image'
+
 
 import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
@@ -16,25 +18,16 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| About Us" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Metaverse</span> is a new
-        thing in the future, where you can enjoy the virtual world by feeling
-        like it's really real, you can feel what you feel in this metaverse
-        world, because this is really the{' '}
-        <span className="font-extrabold text-white">
-          madness of the metaverse
-        </span>{' '}
-        of today, using only{' '}
-        <span className="font-extrabold text-white">VR</span> devices you can
-        easily explore the metaverse world you want, turn your dreams into
-        reality. Let's{' '}
-        <span className="font-extrabold text-white">explore</span> the madness
-        of the metaverse by scrolling down
+        Are you looking to invest in a passive income opportunity that also allows you to be a part of the exciting World of Cryptocurrency? <span className="font-extrabold text-white">Look no further!</span><br /><br />
+        Our new project allows investors to buy miners that mine and earn either  <span className="font-extrabold text-white">ETH</span> on the Ethereum Blockchain or
+        <span className="font-extrabold text-white"> BNB</span> on the Binance Smart Chain.<br />
+        Our state-of-the-art Web3 Technology makes it easy for investors to get started and start earning passive income.
       </motion.p>
 
       <motion.img
