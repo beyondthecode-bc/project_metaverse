c001a1254ea4a2cc91650062b72a9df4a9d13d75
Vlad <62696158+beyondthecode-bc@users.noreply.github.com>
2023-01-13 15:09:54 +0200

    up

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -60,7 +60,6 @@ const Hero = ({ scrollToRef }) => {
                 backgroundSize: "cover",
                 width: '100%',
                 height: '100%'
-
               }}>
                 <ReactPlayer
                   className='react-player fixed-bottom ml-15'
